e57e1cee11714e3d1433ad1d60788e1a89221f70
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-11-17 16:40:47 -0800

    main agent with other agents as tools

---
+++ mainAgent.py
@@ -0,0 +1,88 @@
+from langchain.agents import initialize_agent, AgentType, Tool
+from langchain.chains import LLMMathChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.utilities import SerpAPIWrapper
+from pydantic.v1 import BaseModel, Field
+from langchain.callbacks import HumanApprovalCallbackHandler
+from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.chains import LLMChain
+from langchain.schema.messages import HumanMessage, AIMessage
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+from googleCalendar import *
+from calendarAgent import *
+from typing import Dict
+from datetime import date
+from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.tools import StructuredTool
+from typing_extensions import TypedDict
+
+
+OPENAI_API= os.getenv('OPENAI_API_KEY')
+SERPAPI_API_KEY= os.getenv('SERPAPI_API_KEY')
+
+search = SerpAPIWrapper()
+class SearchInput(BaseModel):
+    query: str = Field(description="should be a search query")
+tools = [
+        Tool(
+            name="Search",
+            func=search.run,
+            description="useful for when you need to answer questions about current events. You should ask targeted questions",
+            args_schema=SearchInput
+        ),
+    ]
+#unable to create agents like the above method as agents take in a specified input of {"input"=userInput}
+
+prompt_template_string= "You are a helpful assistant that manages calendar events. Clarify function arguments if needed. Today's date is {currentDate}. Here are the user's particulars, use it if needed: User's name is {name}, his address is {address}, his/her email is {email}, and his/her phone number is {phone}."
+
+
+def calendar_agent(userInput:str)-> str:
+    """Useful to perform calendar related tasks (create, delete, list events)"""
+    agent= load_calendar_chain_no_memory("gpt-3.5-turbo", user_info)
+    return agent.invoke({"input": userInput})
+
+def doordash_agent(userInput:str)-> str:
+    """Useful to perform doordash related tasks (create order or check on order status)"""
+    agent= load_calendar_chain_no_memory("gpt-3.5-turbo", user_info)
+    return agent.invoke({"input": userInput})
+
+def maps_agent(userInput:str)-> str:
+    """Useful to perform google maps related tasks"""
+    agent= load_calendar_chain_no_memory("gpt-3.5-turbo", user_info)
+    return agent.invoke({"input": userInput})
+
+def news_agent(userInput:str)-> str:
+    """Useful to perform news related tasks"""
+    agent= load_calendar_chain_no_memory("gpt-3.5-turbo", user_info)
+    return agent.invoke({"input": userInput})
+
+tools.append(StructuredTool.from_function(calendar_agent))
+# tools.append(StructuredTool.from_function(doordash_agent))
+# tools.append(StructuredTool.from_function(maps_agent))
+# tools.append(StructuredTool.from_function(news_agent))
+
+
+if __name__ == "__main__":
+    user_info={}
+    user_info["name"]="John Doe"
+    user_info["email"]="JohnDoe@gmail.com"
+    user_info["phone"]="91234567"
+    user_info["location"]="2299 Piedmont Ave, Berkeley, CA 94720"
+
+    prompt_string=prompt_template_string.format(currentDate=date.today().strftime("%B %d, %Y"), name= user_info["name"], address= user_info["location"], email= user_info["email"],phone= user_info["phone"])
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    
+    memory= ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+    chat_history=MessagesPlaceholder(variable_name="chat_history")
+
+    agent=initialize_agent(tools=tools,llm=llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True, agent_kwargs={'prefix': prompt_string,"input_variables": ["chat_history"],"memory_prompts": [chat_history],}, memory= memory)
+
+    response= agent.run("create a calendar event for tea time at 10am tomorrow for 30 minutes at Strate Cafe")
+
+    print(response)
